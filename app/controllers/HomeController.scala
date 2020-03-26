eb2e88ec9ee1b698b69d48f7df1708d22cec0c9c
admin <tadas.vaidotas@gmail.com>
2020-03-26 08:51:03 +0000

    dependency for testing added and an example route

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,8 +6,10 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.index("Hello world"))
   }
+
+
 
 }
